12c22bae2198660e517b9a13f8f9c4c60edbfd60
SamanLotfi <sl01710@srcs.org>
2021-09-20 15:49:36 -0700

    added classes

--- Level1.java
+++ Level1.java
@@ -16,5 +16,16 @@ public class Level1 extends World
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1, false); 
+
+        setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
+            Door.class, HUD.class);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
